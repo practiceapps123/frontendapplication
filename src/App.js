bf5fc2501fa932633c6cb1594b0dad00dca35f3c
Pooja Dutt <pdutt@wisc.edu>
2022-04-01 22:31:54 -0500

    added http protocol and axios:

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      message: ''
+    }
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick () {
+    axios.get('http://localhost:8080/health')
+      .then(response => this.setState({message: response.data.name}))
+  }
+
+  render () {
+    return (
+      <div className='button__container'>
+        <button className='button' onClick={this.handleClick}>Click Me</button>
+        <p>{this.state.message}</p>
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
